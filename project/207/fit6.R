a97d2f58fd21b5be28a60e8d550307d387139045
Clark Fitzgerald <clarkf@gumbel.ucdavis.edu>
2015-02-20 18:31:10 -0800

    used chunking to fit biglm with full model. other methods failed

--- project/207/fit6.R
+++ project/207/fit6.R
@@ -1,7 +1,19 @@
+library(biglm)
+
 source('../functions.R')
 load('../fastrak.Rda')
 
 fastrak = preclean(fastrak)
 
-fit6 = lm(count^(1/3) ~ year + month + weekday*hour + station
-        + station:weekday + station:hour, data=fastrak)
+# Without chunking both lm and biglm will fail when they try to
+# allocate 200 GB vectors
+
+chunksize = as.integer(1e4)
+
+fit6 = biglm(count^(1/3) ~ month + weekday*hour + station + station:weekday + station:hour,
+        data=fastrak[1:chunksize, ])
+
+for (i in 2:floor(nrow(fastrak) / chunksize)){
+    indices = 1:chunksize + (i - 1) * chunksize
+    fit6 = update(fit6, fastrak[indices, ])
+}
