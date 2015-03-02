12e6e7d2439b7b0f41786640a4acc45a18c5195f
Clark Fitzgerald <clarkf@gumbel.ucdavis.edu>
2015-03-01 19:11:51 -0800

    using threading on the server to search through ARMA parameters

---
+++ project/santarosa/arma_fits.R
@@ -0,0 +1,26 @@
+# search through many combinations of ARMA models
+library(parallel)
+
+load('cleaned.Rda')
+
+sr33 = sr3[sr3$group == 3, ]
+sr33$X = scale(sr33$res)
+
+X = sr33$X
+
+# ARMA Parameters to search over:
+ap = expand.grid(ar=0:35, ma=0:35)
+
+getaic = function(ar, ma, x){
+    arima(x, order=c(ar, 0, ma), optim.control=list(maxit=1000))$aic
+}
+
+# Need to run this on a machine with at least ncores available.
+ncores = 20
+cl = makeCluster(ncores)
+
+aicvals = clusterMap(cl, getaic, ap$ar, ap$ma, rep(list(X), nrow(ap)))
+
+ap$aic = unlist(aicvals)
+
+save(ap, file='aic_results.Rda')
